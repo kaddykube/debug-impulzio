361670818a5c482a5873ce811f7ca85d53f07513
kaddykube <148755325+kaddykube@users.noreply.github.com>
2024-05-08 11:26:57 +0200

    Update +page.svelte

--- src/routes/debug/+page.svelte
+++ src/routes/debug/+page.svelte
@@ -1,32 +1,20 @@
 <script lang="ts">
-  import type { PageServerData } from "./$types";
   import TreeView from "$lib/components/debug/tree-view.svelte";
   import DebugForm from "$lib/components/debug/debug-form.svelte";
-  import type {  YEAR } from "$lib/types/debug";
+  import type { Calculation, YEAR } from "$lib/types/debug";
   import TreeFilter from "$lib/components/debug/tree-filter.svelte";
 
-  export let data: PageServerData;
-
-
-  let treeContent = data.liqui.map((value) => {
-    return {
-      name: Number(value.year),
-      children: [
-        {
-          name: "Berechnung Monate",
-          children: value.months
-            ? value.months.map((m: { [key: string]: any }) => {
-                return {
-                  name: m.month,
-                  cols: m,
-                };
-              })
-            : null,
-          cols: { name: "Berechnung Jahr", cols: value.calculation },
-        },
-      ],
-    };
-  });
+  let treeContent: {
+    name: number;
+    children: {
+      name: string;
+      children: any;
+      cols: {
+        name: string;
+        cols: Calculation;
+      };
+    }[];
+  }[] | undefined;
 
   function handleSubmit(
     event: CustomEvent<{
@@ -54,22 +42,30 @@
       };
     });
   }
-
 </script>
+
 <div class="flex gap-10">
   <div class="w-3/4 flex-col flex max-h-[90vh]">
     <p class="text-lg pl-2">Berechnungen Steuer&Liqui</p>
     <TreeFilter></TreeFilter>
     <div
-      class="border-2 border-dark-10 rounded-3xl px-10 pb-10 mb-2 overflow-y-auto max-h-[70vh]"
+      class="border-2 border-dark-10 rounded-3xl overflow-hidden max-h-[70vh]"
     >
-      <TreeView tree_data={treeContent} filter/>
+      <div class="overflow-y-auto h-full px-10 pb-10 mb-2">
+        {#if treeContent}
+          <TreeView tree_data={treeContent} filter />
+        {:else}
+          <p class="text-sm mt-10">
+            Bitte füge zuerst Kennzahlen und Mitarbeiter hinzu
+          </p>
+        {/if}
+      </div>
     </div>
-    <p class="text-lg pl-2">Auswertung Mitarbeiter</p>
+    <p class="text-lg pl-2 mt-4">Auswertung Mitarbeiter</p>
     <div
       class="border-2 border-dark-10 rounded-3xl px-10 pb-10 overflow-y-auto h-[20vh]"
     >
-    <TreeView tree_data={[]} filter={false}/>
+      <TreeView tree_data={[]} filter={false} />
     </div>
   </div>
 
